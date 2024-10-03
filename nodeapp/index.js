9ce0fb7510a3321634dfa0a5e0edfe9a274f0239
Dasun Theekshana Abeysinghe Wijesekara <129314655+DTA-Wijesekara@users.noreply.github.com>
2024-10-04 02:15:50 +0530

    updated

--- nodeapp/index.js
+++ nodeapp/index.js
@@ -2,7 +2,7 @@ var express = require("express");
 var app = express();
 
 app.get("/", function (req, res) {
-  res.send('{ "response": "Hello, Welcome to Sri Lanka" }');
+  res.send('{ "response": "Hello, Welcome to University of Moratuwa" }');
 });
 
 app.get("/will", function (req, res) {
